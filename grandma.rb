9bed25e4985f9a0ce452dd502584c3386a53e76b
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-09 18:19:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO NOT SINCE 1938"
-  else 
+  else
     return "HUH?! SPEAK UP, SONNY"
 
 # If you shout, she can hear you (or at least she thinks so)
