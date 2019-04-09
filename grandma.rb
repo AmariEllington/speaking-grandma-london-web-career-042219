2e4d209a7c875e9629c7c0277939a6bdf2cdd86a
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-09 18:19:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,6 +10,8 @@ def speak_to_grandma(phrase)
   else
     return "HUH?! SPEAK UP, SONNY"
 
+end
+
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
@@ -17,4 +19,3 @@ def speak_to_grandma(phrase)
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-end
