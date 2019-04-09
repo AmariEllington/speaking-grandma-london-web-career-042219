15a19a2fa704500640c358d082cf6bfb18743798
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-09 18:12:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase === "I LOVE YOU GRANDMA"
     return "I LOVE YOU TOO PUMPKIN!"
-
+  elsif phrase === phrase.downcase
+    return "HUH?! SPEAK UP SONNY!"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
